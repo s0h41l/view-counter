6a07456aa0743968007553ace1498b1b2fed6d51
Sohail <kh4nz4d4@gmail.com>
2021-01-23 00:25:36 +0500

    counter changed

--- routes/main.js
+++ routes/main.js
@@ -6,6 +6,6 @@ const { getPage, getCode, getAllCodes } = require('../controllers/main');
 
 router.get('/', getPage);
 router.post('/getCode', getCode);
-router.post('/all', getAllCodes);
+router.get('/all', getAllCodes);
 
 module.exports = router;
